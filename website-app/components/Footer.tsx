fc68c693e0da4229cf2943d9ee2e6de5853f4eb4
Tata0703 <danqing.zhang.personal@gmail.com>
2025-03-08 22:28:15 -0800

    add social media links in the footer

--- website-app/components/Footer.tsx
+++ website-app/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { FaLinkedin, FaTwitter, FaDiscord, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
     return (
@@ -12,8 +13,9 @@ const Footer = () => {
               Privacy Policy
             </Link>
           </div>
+          
           <div
-            className="text-center text-sm text-gray-800 dark:text-gray-400"
+            className="text-center text-sm text-gray-800 dark:text-gray-400 mb-4 md:mb-0"
           >
             © {new Date().getFullYear()}, Built by
             {` `}
@@ -22,6 +24,45 @@ const Footer = () => {
               className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-600"
             >
               PathOnAI.org
+            </a>
+          </div>
+          
+          <div className="flex items-center space-x-4">
+            <a
+              href="https://github.com/PathOnAI"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-600"
+              aria-label="GitHub"
+            >
+              <FaGithub className="h-5 w-5" />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/pathonai/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-600"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin className="h-5 w-5" />
+            </a>
+            <a
+              href="https://x.com/PathOnAI"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-600"
+              aria-label="Twitter"
+            >
+              <FaTwitter className="h-5 w-5" />
+            </a>
+            <a
+              href="https://discord.com/invite/UTxjyNwTeP"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-600"
+              aria-label="Discord"
+            >
+              <FaDiscord className="h-5 w-5" />
             </a>
           </div>
         </div>
